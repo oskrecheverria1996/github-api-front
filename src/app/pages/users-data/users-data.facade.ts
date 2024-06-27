43c717670f18c5b4b12454298d33d48b8ae11965
oskrecheverria1996 <oskr.echeverria1996@gmail.com>
2024-06-27 17:04:31 -0500

    @master - add open-api services

--- src/app/pages/users-data/users-data.facade.ts
+++ src/app/pages/users-data/users-data.facade.ts
@@ -4,24 +4,29 @@ import { UsersDataState } from "./users-data.state";
 import { finalize, map, tap } from "rxjs/operators";
 import { Observable } from "rxjs";
 import { NotificationsService } from "../../shared/common/notifications.service";
+import { SearchService, UsersService } from "src/app/api/github-service/services";
 
 @Injectable()
 export class UsersDataFacade {
     constructor(
         private apiServicesService: ApiServicesService,
         private usersDataState: UsersDataState,
-        private notificationsService: NotificationsService
+        private notificationsService: NotificationsService,
+        private searchService: SearchService,
+        private usersService: UsersService
     ) { }
 
     
-    getListUsers(name?) {
+    getListUsers(q?) {
         this.usersDataState.setLoading(true)
-        this.apiServicesService.getUsersList(name)
+        let per_page = 10;
+        // this.apiServicesService.getUsersList(name)
+        this.searchService.searchUsers({q, per_page})
         .pipe(finalize(() => this.usersDataState.setLoading(false)))
-        .pipe(map(x => x.items.slice(0, 10)))
+        // .pipe(map(x => x.items.slice(0, 10)))
         .subscribe(
             (res) => {
-                this.usersDataState.setListUsers(res);
+                this.usersDataState.setListUsers(res.items);
             },
             (err) => {
                 this.notificationsService.error(err.error.message, 'Error');
@@ -29,9 +34,10 @@ export class UsersDataFacade {
         )
     }
        
-    getUserByName(name) {
-        this.usersDataState.setLoadingSingle(true)
-        this.apiServicesService.getUserByName(name)
+    getUserByName(username) {
+        this.usersDataState.setLoadingSingle(true);
+        // this.apiServicesService.getUserByName(name)
+        this.usersService.usersGetByUsername({username})
         .pipe(finalize(() => this.usersDataState.setLoadingSingle(false)))
         .subscribe(
             (res) => {
